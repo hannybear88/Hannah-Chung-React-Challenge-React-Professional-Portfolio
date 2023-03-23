2264b9ccc514eeb411bc9d3d3a33b5792ad6687d
hannybear88 <hannahkchung88@gmail.com>
2023-03-23 04:42:55 -0700

    project 2 and 3 links are not working and I'm not sure why. Also, deleted more commented out code

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,7 +10,7 @@ import { validateEmail } from '../utils/helper';
 
 
 function Contact() {
-  // const [state, handleSubmit] = useForm({});
+
 
   const [formState, setFormState] = useState({ name: '', email: '', message: '' });
   const [isSuccess, setIsSuccess] = useState(false);
@@ -28,13 +28,6 @@ function Contact() {
       setIsSuccess(true)
     }
 
-  //   if (!errorMessage) {
-  //     console.log('Submit Form', formState);
-  //   }
-  // };
-
-  // function refreshPage() {
-  //   window.location.reload(true);
   }
 
 
@@ -99,122 +92,3 @@ function Contact() {
 
 export default Contact;
 
-// import { useState } from "react";
-// import { Container, Row, Col } from "react-bootstrap";
-// import contactImg from "../assets/img/hand-img.png";
-// import "animate.css";
-// import TrackVisibility from "react-on-screen";
-
-// export const Contact = () => {
-//   const formInitialDetails = {
-//     name: "",
-//     email: "",
-//     message: "",
-//   };
-//   const [formDetails, setFormDetails] = useState(formInitialDetails);
-//   // eslint-disable-next-line no-unused-vars
-//   const [buttonText, setButtonText] = useState("Send");
-//   // eslint-disable-next-line no-unused-vars
-//   const [status, setStatus] = useState({});
-
-//   const onFormUpdate = (e) => {
-//     setFormDetails({
-//       ...formDetails,
-//       [e.target.name]: e.target.value,
-//     });
-//   };
-
-// return (
-//   <section className="contact" id="connect">
-//     <Container>
-//       <Row className="align-items-center">
-//         <Col size={12} md={6}>
-//           <TrackVisibility>
-//             {({ isVisible }) => (
-//               <img
-//                 className={
-//                   isVisible ? "animate__animated animate__zoomIn" : ""
-//                 }
-//                 src={contactImg}
-//                 alt="hands"
-//               />
-//             )}
-//           </TrackVisibility>
-//         </Col>
-//         <Col size={12} md={6}>
-//           <TrackVisibility>
-//             {({ isVisible }) => (
-//               <div
-//                 className={
-//                   isVisible ? "animate__animated animate__fadeIn" : ""
-//                 }
-//               >
-//                 <h2>Get In Touch</h2>
-//                 <form
-//                   action="https://formsubmit.co/hannahkchung88@gmail.com"
-//                   method="POST"
-//                 >
-//                   <Row>
-//                     <Col size={12} sm={6} className="px-1">
-//                       <input
-//                         name="name"
-//                         type="text"
-//                         value={formDetails.name}
-//                         placeholder="Name"
-//                         onChange={onFormUpdate}
-//                         required
-//                       />
-//                     </Col>
-//                     <Col size={12} sm={6} className="px-1">
-//                       <input
-//                         name="email"
-//                         type="email"
-//                         value={formDetails.email}
-//                         placeholder="Email Address"
-//                         onChange={onFormUpdate}
-//                         required
-//                       />
-//                     </Col>
-//                     <Col size={12} className="px-1">
-//                       <textarea
-//                         name="message"
-//                         rows="6"
-//                         value={formDetails.message}
-//                         placeholder="Message"
-//                         onChange={onFormUpdate}
-//                         required
-//                       ></textarea>
-//                       <button type="submit">
-//                         <span>{buttonText}</span>
-//                       </button>
-//                     </Col>
-//                     <input
-//                       name="_url"
-//                       type="hidden"
-//                       value="https://github.com/hannybear88"
-//                     />
-//                     {status.message && (
-//                       <Col>
-//                         <p
-//                           className={
-//                             status.success === false ? "danger" : "success"
-//                           }
-//                         >
-//                           {status.message}
-//                         </p>
-//                       </Col>
-//                     )}
-//                   </Row>
-//                 </form>
-//               </div>
-//             )}
-//           </TrackVisibility>
-//         </Col>
-//       </Row>
-//     </Container>
-//   </section>
-// );
-// };
-
-
-// export default Contact;
