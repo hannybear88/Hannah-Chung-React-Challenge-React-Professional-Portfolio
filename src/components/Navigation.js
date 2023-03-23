3ee90754608b531e2accd3ecb3699ed371f4843a
hannybear88 <hannahkchung88@gmail.com>
2023-03-23 04:35:51 -0700

    deleted commented out code

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -76,46 +76,6 @@ function Navigation(props) {
         </div>
       </div>
     </nav>
-    // <header>
-    //   <div class="name">
-    //     <h3>Hannah Kim Chung</h3>
-    //   </div>
-
-    //   <nav>
-    //     <ul>
-    //       <li
-    //         className = {props.selectedPage == "About" ? "selected" : ""}
-    //         onClick={() => {
-    //           props.setSelectedPage("About");
-    //         }}
-    //       >
-    //         <a href="#About Me">AboutMe</a>
-    //       </li>
-    //       <li
-    //       className = {props.selectedPage == "Work" ? "selected" : ""}
-    //         onClick={() => {
-    //           props.setSelectedPage("Work");
-    //         }}
-    //       >
-    //         <a href="#Work">Projects</a>
-    //       </li>
-    //       <li
-    //       className = {props.selectedPage == "Resume" ? "selected" : ""}
-    //         onClick={() => {
-    //           props.setSelectedPage("Resume");
-    //         }}>
-    //         <a href="#Resume">Resume</a>
-    //       </li>
-    //       <li
-    //       className = {props.selectedPage == "Contact" ? "selected" : ""}
-    //         onClick={() => {
-    //           props.setSelectedPage("Contact");
-    //         }}>
-    //         <a href="#Contact Me">Contact Me</a>
-    //       </li>
-    //     </ul>
-    //   </nav>
-    // </header>
   );
 }
 
